2ac659b34b156b1a83d66bbd55e4bc19a78a3be7
aoao84951-png <aoao84951@gmail.com>
2026-09-10 01:49:55 +0900

    Use signature pink for PWA theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   viewportFit: 'cover',
-  themeColor: '#ffffff',
+  themeColor: '#c98298',
 };
 
 export default function RootLayout({
