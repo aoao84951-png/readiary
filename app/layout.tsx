aa2e380794211429926d04385c564dfe4aaa9b79
aoao84951-png <aoao84951@gmail.com>
2026-09-10 01:53:12 +0900

    Soften PWA theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   viewportFit: 'cover',
-  themeColor: '#f1a8c1',
+  themeColor: '#fde7ef',
 };
 
 export default function RootLayout({
